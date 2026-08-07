05dace714ba5cb384e36b3955be2d7f0ad546dd0
gynsys <dramarielh@gmail.com>
2026-08-07 13:00:58 -0400

    Restore auth routes

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -8,10 +8,12 @@ from app.api.v1.endpoints import materials
 from app.api.v1.endpoints import cost360
 from app.api.v1.endpoints import search_v6
 from app.api.v1.endpoints import uploads
+from app.api.v1.endpoints import arko
 
 api_router = APIRouter()
 
 # APUpro Endpoints
+api_router.include_router(arko.router, prefix="/arko", tags=["arko360"])
 api_router.include_router(cost360.router, prefix="/cost360", tags=["cost360"])
 api_router.include_router(search_v6.router, prefix="/cost360/v6", tags=["cost360_v6"])
 api_router.include_router(budgets.router, prefix="/budgets", tags=["budgets"])
