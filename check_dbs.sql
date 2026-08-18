8debcad635e3a47ddacff26079e49ab29676e937
gynsys <dramarielh@gmail.com>
2026-08-18 09:44:52 -0400

    Actualizacion

---
+++ check_dbs.sql
@@ -0,0 +1,1 @@
+SELECT id, name, description, is_master, is_active FROM public.cost360_databases ORDER BY created_at;
---
+++ fix_dbs.sql
@@ -0,0 +1,2 @@
+UPDATE public.cost360_databases SET is_master = FALSE, is_active = TRUE WHERE id = 'temp_u';
+UPDATE public.cost360_databases SET is_master = FALSE, is_active = TRUE WHERE id = 'temp_m';
