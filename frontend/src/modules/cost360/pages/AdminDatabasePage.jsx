d3b347ebf1a304667cb8b42c36a5f925bcc6d857
gynsys <dramarielh@gmail.com>
2026-08-28 13:56:04 -0400

    Actualizacion

--- frontend/src/modules/cost360/pages/AdminDatabasePage.jsx
+++ frontend/src/modules/cost360/pages/AdminDatabasePage.jsx
@@ -35,22 +35,6 @@ const ScrapingDashboard = () => {
 };
 
 const ModuloSincronizacionCostos = () => {
-  const [botStatus, setBotStatus] = useState('idle');
-
-  return (
-    <div className="flex flex-col gap-4 h-full">
-      <ControlBar status={botStatus} onStatusChange={setBotStatus} />
-      <div className="flex gap-4 flex-1 min-h-0">
-        <div className="flex-1">
-          <LogConsole />
-        </div>
-        <div className="w-80">
-          <ConfigPanel />
-        </div>
-      </div>
-    </div>
-  );
-};
   const [estaProcesando, setEstaProcesando] = useState(false);
   const [pendingItems, setPendingItems] = useState([]);
   const [loadingPending, setLoadingPending] = useState(false);
