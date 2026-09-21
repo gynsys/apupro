237091bd756b284b48891b7af06dd1faacc13c46
gynsys <dramarielh@gmail.com>
2026-09-21 08:12:27 -0400

    Corregir import Any en schemas costbase

--- backend/app/schemas/costbase.py
+++ backend/app/schemas/costbase.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Any
 from datetime import datetime
 
 class CostItemBase(BaseModel):
